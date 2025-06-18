6deb2fcc12db5fe4a719d9f102d02194ff44101a
Aakash Mahajan <mahajanaakash108@gmail.com>
2025-06-18 17:39:12 +0530

    feat: added short selling too

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from backtesting import Backtest, Strategy
+from backtesting.lib import crossover
 import talib
 from backtesting.test import GOOG
 
 class GridTradingStrategy(Strategy):
+    EMA_SHORT_PERIOD = 9
+    EMA_LONG_PERIOD = 21
     ATR_PERIOD = 14
     NUMBER_OF_LEVELS = 5
     STOP_LOSS_FACTOR = 1.2
     TAKE_PROFIT_FACTOR = 0.25
     
     def init(self):
+        self.ema_short = self.I(talib.EMA, self.data.Close, self.EMA_SHORT_PERIOD)
+        self.ema_long = self.I(talib.EMA, self.data.Close, self.EMA_LONG_PERIOD)
         self.atr = self.I(talib.ATR, self.data.High, self.data.Low, self.data.Close, self.ATR_PERIOD)
         self.active = False
         self.traded_ltp = -1
@@ -19,38 +24,73 @@ class GridTradingStrategy(Strategy):
         self.traded_prices = []  
     
     def next(self):
-        atr = self.atr[-1]
-        ltp = self.data.Close[-1]
+        if crossover(self.ema_short, self.ema_long):
+            atr = self.atr[-1]
+            ltp = self.data.Close[-1]
         
-        if not self.position:
-            self.active = True
-            self.traded_ltp = ltp
-            self.traded_atr = atr
-            self.sl = self.traded_ltp - (self.STOP_LOSS_FACTOR * self.traded_atr)
-            self.tp = self.traded_ltp + (self.TAKE_PROFIT_FACTOR * self.traded_atr)
+            if not self.position:
+                self.active = True
+                self.traded_ltp = ltp
+                self.traded_atr = atr
+                self.sl = self.traded_ltp - (self.STOP_LOSS_FACTOR * self.traded_atr)
+                self.tp = self.traded_ltp + (self.TAKE_PROFIT_FACTOR * self.traded_atr)
             
-            self.buy(size=1, sl=self.sl, tp=self.tp)
-            self.traded_prices.append(ltp)  
+                self.buy(size=1, sl=self.sl, tp=self.tp)
+                self.traded_prices.append(ltp)  
             
-        else:
-            for level in range(self.NUMBER_OF_LEVELS):
-                price_trigger = self.traded_ltp - (0.2 * level * self.traded_atr)
-                if (ltp < price_trigger):
-                    self.level += 1
-                    size = 2 ** self.level  
-                    self.buy(size=size, sl=self.sl, tp=self.tp)
-                    self.traded_prices.append(ltp)  
-                    break
+            else:
+                for level in range(self.NUMBER_OF_LEVELS):
+                    price_trigger = self.traded_ltp - (0.2 * level * self.traded_atr)
+                    if (ltp < price_trigger):
+                        self.level += 1
+                        size = 2 ** self.level  
+                        self.buy(size=size, sl=self.sl, tp=self.tp)
+                        self.traded_prices.append(ltp)  
+                        break
         
-        if len(self.traded_prices) != 0:
-            average_traded_price = sum(self.traded_prices) / len(self.traded_prices) 
-            if (self.level > (self.NUMBER_OF_LEVELS/2)) and (ltp > average_traded_price):
-                self.position.close()
-                self.level = 1
-                self.active = False
-                self.traded_prices = []  
+            if len(self.traded_prices) != 0:
+                average_traded_price = sum(self.traded_prices) / len(self.traded_prices) 
+                if (self.level > (self.NUMBER_OF_LEVELS/2)) and (ltp > average_traded_price):
+                    self.position.close()
+                    self.level = 1
+                    self.active = False
+                    self.traded_prices = []  
+                    
+                    
+        elif crossover(self.ema_long, self.ema_short):
+            atr = self.atr[-1]
+            ltp = self.data.Close[-1]
+        
+            if not self.position:
+                self.active = True
+                self.traded_ltp = ltp
+                self.traded_atr = atr
+                self.sl = self.traded_ltp + (self.STOP_LOSS_FACTOR * self.traded_atr)
+                self.tp = self.traded_ltp - (self.TAKE_PROFIT_FACTOR * self.traded_atr)
             
-       
+                self.sell(size=1, sl=self.sl, tp=self.tp)
+                self.traded_prices.append(ltp)  
+            
+            else:
+                for level in range(self.NUMBER_OF_LEVELS):
+                    price_trigger = self.traded_ltp + (0.2 * level * self.traded_atr)
+                    if (ltp > price_trigger):
+                        self.level += 1
+                        size = 2 ** self.level  
+                        self.sell(size=size, sl=self.sl, tp=self.tp)
+                        self.traded_prices.append(ltp)  
+                        break
+        
+            if len(self.traded_prices) != 0:
+                average_traded_price = sum(self.traded_prices) / len(self.traded_prices) 
+                if (self.level > (self.NUMBER_OF_LEVELS/2)) and (ltp < average_traded_price):
+                    self.position.close()
+                    self.level = 1
+                    self.active = False
+                    self.traded_prices = []  
+            
+        
+           
         
 bt = Backtest(GOOG, GridTradingStrategy, cash=10_000, commission=0.002)    
 stats = bt.run()
